488093cc3dbdd5ed1fbfba523bb0de7104beb79e
Dana2021 <114039011+Dmonzer@users.noreply.github.com>
2024-12-04 18:03:38 -0600

    updating the download function

--- db_test.py
+++ db_test.py
@@ -398,6 +398,17 @@ def nearby_birds(baseurl):
         # }
 #
 #
+## Helper function 1 - create textfile 
+
+def create_text_file(trip_data):
+    content = ""
+    for key, value in trip_data.items():
+        content += f"{'-' * 30}\n{key}\n{'-' * 30}\n    {value}\n\n" 
+    current_directory = os.getcwd()
+    file_path = os.path.join(current_directory, "trip_data.txt")
+    with open(file_path, "w") as file:
+        file.write(content)
+    return file_path
 
 def download_trip(baseurl):
     """
@@ -447,6 +458,27 @@ def download_trip(baseurl):
                 body = res.json()
                 print("Error message:", body)
             return
+        
+       
+        
+        #if output was list
+        #row = res.json()
+        '''
+        trip_data = {
+            "id": row[0],
+            "bird_name": row[1],
+            "start_loc": row[2],
+            "end_loc": row[3],
+            "trans_mode": row[4],
+            "distance": row[5],
+            "instructions": row[6]
+        }
+        '''
+        trip_data = res.json()
+        file_path = create_text_file(trip_data)
+
+        print(file_path)
+        return
 
     except Exception as e:
         logging.error("**ERROR: nearby_birds() failed:")
